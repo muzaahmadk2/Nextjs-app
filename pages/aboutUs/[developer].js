a85fbbf0671366991074d0fd73c6c57643db9f12
muzaahmadk2 <muzaahmadk2@gmail.com>
2023-08-17 22:23:36 +0530

    dynamic routes

---
+++ pages/aboutUs/[developer].js
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+const details = [
+  { "id": 1, "name": "Yash", "role": "Senior Developer" },
+  { "id": 2, "name": "Vaibhav", "role": "Backend Developer" },
+  { "id": 3, "name": "Suresh", "role": "Frontend Developer" },
+];
+
+function DeveloperPage() {
+  const router = useRouter();
+  const id = router.query.developer;
+    let deve = details.find(itm => {
+        return itm.id == id;
+    })
+
+
+console.log(deve)
+  return  <h1>This is {deve.name} {deve.role}</h1>
+}
+
+export default DeveloperPage;
